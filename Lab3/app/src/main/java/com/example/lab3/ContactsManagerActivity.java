164882f0182a7f3d6486ae81dd8e0e916cacaefa
onelessangel <69592470+onelessangel@users.noreply.github.com>
2024-03-21 19:50:32 +0200

    Added ContactsManager UI

---
+++ Lab3/app/src/main/java/com/example/lab3/ContactsManagerActivity.java
@@ -0,0 +1,28 @@
+package com.example.lab3;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ContactsManagerActivity extends AppCompatActivity {
+    Button detailsButton, cancelButton, saveButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_contacts_manager);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+    }
+}
